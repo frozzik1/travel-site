5aa412c2d529e092ef0f8f03a5fec3af3cc846a5
frozzik1 <86525613+frozzik1@users.noreply.github.com>
2023-12-01 10:50:01 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,19 +10,33 @@ window.addEventListener('scroll', () => {
 document.addEventListener("DOMContentLoaded", function () {
     var form = document.querySelector(".main_page_btn");
     form.addEventListener("submit", function (e) {
-      e.preventDefault();
-      var targetId = this.getAttribute("action");
-      var targetElement = document.querySelector(targetId);
-      window.scrollTo({
-        top: targetElement.offsetTop,
-        behavior: "smooth",
+        e.preventDefault();
+        var targetId = this.getAttribute("action");
+        var targetElement = document.querySelector(targetId);
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: "smooth",
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollBtns = document.querySelectorAll(".menu_a");
+    scrollBtns.forEach((scrollBtn) => {
+      scrollBtn.addEventListener("click", function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute("href");
+        const targetElement = document.querySelector(targetId);
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: "smooth",
+        });
       });
     });
   });
-  
 
 function initMap() {
-    var myLatLng = { lat: 50.00651325201618, lng: 36.23763404345009 }; 
+    var myLatLng = { lat: 50.00633084759501, lng: 36.23747793159957 };
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 14,
         center: myLatLng
@@ -30,7 +44,7 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'Hello World!'
+        title: 'Golden Travel'
     });
 }
 
