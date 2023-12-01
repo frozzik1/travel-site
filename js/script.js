6952fba607cde7e3475c58c1d58ecfc022ead43b
frozzik1 <86525613+frozzik1@users.noreply.github.com>
2023-12-01 10:28:09 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,18 @@
- const header = document.querySelector('.scroll_here');
-const headerImage = document.querySelector('.logo');
+const header = document.querySelector('.scroll_here');
 window.addEventListener('scroll', () => {
     if (window.scrollY > 920) {
         header.classList.add('scrolled');
-        headerImage.src = 'images/logo2.png';
-
     } else {
         header.classList.remove('scrolled');
-        headerImage.src = 'images/logo.png';
     }
 });
 
 document.addEventListener("DOMContentLoaded", function () {
     var form = document.querySelector(".main_page_btn");
-  
     form.addEventListener("submit", function (e) {
       e.preventDefault();
-  
       var targetId = this.getAttribute("action");
       var targetElement = document.querySelector(targetId);
-  
       window.scrollTo({
         top: targetElement.offsetTop,
         behavior: "smooth",
@@ -29,14 +22,11 @@ document.addEventListener("DOMContentLoaded", function () {
   
 
 function initMap() {
-
     var myLatLng = { lat: 50.00651325201618, lng: 36.23763404345009 }; 
-
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 14,
         center: myLatLng
     });
-
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
